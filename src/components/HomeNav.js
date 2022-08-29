61270411649acb31b49cf09b773248ee746d1f3b
Moshood Sanusi <snsmoshood@gmail.com>
2022-08-29 18:48:47 +0100

    setting things up

---
+++ src/components/HomeNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeNav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeNav
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomeNav from './HomeNav'
+import LoginSignup from './LoginSignup'
+
+const HomePage = () => {
+  return (
+    <>
+
+      <LoginSignup />
+    </>
+  )
+}
+
+export default HomePage
