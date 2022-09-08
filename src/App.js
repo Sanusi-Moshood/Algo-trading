810130761ef2b777feda9efbace01d0abc4faf0f
Moshood Sanusi <snsmoshood@gmail.com>
2022-09-08 15:34:22 +0100

    quick fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Account/Login'
 import Register from './pages/Account/Register';
-import Register2 from './pages/Account/Register2';
 import Forgotpass from './pages/Account/Forgotpass';
 import './index.css'
 import Dashboard from './pages/Dashboard'
@@ -26,7 +25,6 @@ function App() {
         <BrowserRouter>
         <Routes>
           <Route path='/register'  element={<Register />}/>
-          <Route path='/register2'  element={<Register2 />}/>
           <Route path='/' element={<Login/>}/>
           <Route path='/forgot_password'  element={<Forgotpass />}/>      
         </Routes>
