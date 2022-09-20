97591e1e3a047a5c46123d092dd0329420b535d9
Tez-cyber <75941578+Tez-cyber@users.noreply.github.com>
2022-09-20 16:38:36 -0700

    Add files via upload

---
+++ DashboardPage.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { AccountContext } from '../context/Account'
+import styles from './dashboard.module.css'
+import { useContext,useState, useEffect } from 'react'
+import axios from 'axios'
+import { 
+  MDBTable, MDBTableHead,
+  MDBTableBody, MDBContainer,
+  MDBRow, MDBCol
+ } from 'mdb-react-ui-kit'
+
+
+const DashboardPage = () => {
+    const {userData} = useContext(AccountContext)
+    const [sort, setSort]=useState('All')
+    const [data, setData] = useState([]);
+
+    //-----------------------------------------
+    useEffect(() => {
+      getAllData();
+    }, []);
+
+    //------------------------------------------
+    const getAllData = async () => {
+      return await axios
+      .get(" https://copytraderapi.fnoalgo.com/orders/tradeorders/1383/all")
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+    };
+    console.log(data);
+    
+  return (
+    // <div className={styles.dashPage}>
+    //   <h1>Orders Status</h1>
+    //   <div className={styles.sort}>
+    //   <p>Sort by {sort}</p> 
+    //   <select  value={sort} onChange={e=>setSort(e.target.value)}>
+    //   <option>All</option>
+    //   <option>Group</option>
+    //   <option>Account</option>
+    //   <option>Order Id</option>
+    //   </select>
+    //   </div>
+    // </div>
+
+// ********************** AZEEZ CREATE A TABLE COMPONENT HERE 
+//*********************** AND INSIDE IT YOU SHOULD CREATE A DUMMY TABLE WITH ALL INFO 
+//*********************** I'LL ADD PAGINATION, MAKE API CALL AND MAP OVER THE DATA */
+
+  <MDBRow>
+  <MDBCol size="12">
+    <MDBTable>
+      <MDBTableHead dark>
+        <tr>
+          <th scope="col">No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Address</th>
+          <th scope="col">Status</th>
+        </tr>
+      </MDBTableHead>
+      {data.length === 0 ?
+        (
+          <MDBTableBody className='align-center mb-0'>
+            <tr>
+              <td className="text-center mb-0" colSpan={8}>No Data Found</td>
+            </tr>
+          </MDBTableBody>
+        ) : (
+          data.map((item, index) => (
+            <MDBTableBody key={index}>
+              <tr>
+                <th scope='row'>{index+1}</th>
+                <td> {item.name} </td>
+                <td> {item.email} </td>
+                <td> {item.phone} </td>
+                <td> {item.address} </td>
+                <td> {item.status} </td>
+              </tr>
+            </MDBTableBody>
+          ))
+        )
+      }
+    </MDBTable>
+  </MDBCol>
+  </MDBRow>
+
+  )
+}
+
+export default DashboardPage
